e570c6b77e56669c1709dc8dc7bea4788044261f
Daniel Szabo <daniel_awesome1996@hotmail.com>
2023-03-17 23:00:51 +1030

    feat: turn project into gradle spring boot [N/A]

---
+++ src/main/java/com/github/zodius/service/f1characters/F1CharactersApplication.java
@@ -0,0 +1,13 @@
+package com.github.zodius.service.f1characters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1CharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1CharactersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zodius/service/f1characters/F1CharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zodius.service.f1characters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1CharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
